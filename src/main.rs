9edb26e0d4f7c82c343d73d8bd4220b954a2bfaa
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-04-25 13:19:21 -0400

    feat: clap CLI parsing, escape parsing
    Fully working echo utility with CLI parsing via the clap crate. Abililty
    to remove newline from echoed output, to explicitly disable escape
    characters (default), and to enable them.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
-use std::env;
+use clap::Parser;
+
+#[derive(Parser,Debug)]
+#[command(name="recho", version, about="A simple echo-like CLI took written in Rust")]
+struct Args {
+    /// Remove trailing newline
+    #[arg(short = 'n', long)]
+    no_newline: bool,
+
+    /// Interpret escape sequence characters
+    #[arg(short = 'e', long, overrides_with = "no_escape")]
+    escape: bool,
+
+    /// Disable interpretation of backslash escapes (default)
+    #[arg(short = 'E', long, overrides_with = "escape")]
+    no_escape: bool,
+
+    /// String to echo 
+    #[arg()]
+    text: Vec<String>,
+
+}
 
 fn main() {
-    // Retrieve arguments from CLI
-    let args: Vec<String> = env::args().skip(1).collect(); 
+    // Retrieve clap-defined args
+    let args = Args::parse();
 
-    // Join "args" together
-    // Assuming no flags at present
-    let echo = args.join(" ");
+    // Join remainder or arguments -> what to echo
+    let mut echo = args.text.join(" ");
 
-    // Return result to stdout
-    println!("{}", echo);
+    // Parse escapes if escape flag present
+    if args.escape {
+        echo = parse_escapes(&echo);
+    } 
+
+    // Check for newline removal and 
+    // return result to stdout
+    if args.no_newline {
+        print!("{}", echo);
+    } else {
+        println!("{}", echo);
+    }
+}
+
+// Parses escape characters for provided echo output
+fn parse_escapes(output: &str) -> String {
+    output
+        .replace("\\\\", "\\") // backslash
+        .replace("\\n", "\n")   // newline
+        .replace("\\r", "\r")   // carriage return
+        .replace("\\t", "\t")   // tab
+        .replace("\\\"", "\"")  // double quote
 }
